d57480310c67b4e4de0b522c6944ff7ae9127ad1
Brian McLaughlin <brian.mclaughlin@slcontrols.com>
2020-05-02 15:10:47 +0100

    recommiting user interaction functionality

--- NumberGuesser/NumberGuesser/Program.cs
+++ NumberGuesser/NumberGuesser/Program.cs
@@ -8,11 +8,30 @@ namespace NumberGuesser
         // Entry point for the appliacation
         static void Main(string[] args)
         {
-            string name = "Brian Mc Laughlin";
-            int age = 35;
-            //Code start here
-            Console.WriteLine("Hello " + name + "!" + " You are " + age + "!");
-            Console.WriteLine("Hello {0} ! You are {1} !", name, age);
+            // Set App Vars
+            string appName = "Number Guesser";
+            string appVersion = "Version 1.0.0";
+            string appAuthor = "Brian McLaughlin";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            //Console.BackgroundColor = ConsoleColor.Gray; 
+
+            Console.WriteLine("{0} : {1} : {2}", appName, appVersion, appAuthor);
+            Console.ResetColor();
+
+            Console.WriteLine("what is your name?");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Hello {0} lets play number guesser!", input);
+
+            int correct_number = 7;
+            int guess_number = 0;
+
+            Console.WriteLine("Guess a number between 1 and 10");
+
+
+
         }
     }
 }
