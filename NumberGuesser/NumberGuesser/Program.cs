090d5d948ccdbf7185efcf2110ef5385ca796758
Brian McLaughlin <brian.mclaughlin@slcontrols.com>
2020-05-02 14:28:57 +0100

    initial commit - string example

---
+++ NumberGuesser/NumberGuesser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+// Namesace
+namespace NumberGuesser
+{
+    //MainClass
+    class Program
+    {
+        // Entry point for the appliacation
+        static void Main(string[] args)
+        {
+            string name = "Brian Mc Laughlin";
+            int age = 35;
+            //Code start here
+            Console.WriteLine("Hello " + name + "!" + " You are " + age + "!");
+            Console.WriteLine("Hello {0} ! You are {1} !", name, age);
+        }
+    }
+}
